dc533df8fc2b6d32de6d8ac74dab9d177134ec67
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2021-07-19 19:09:25 -0300

    Commit inicial T3

---
+++ Trabalho3/sources/funcionalidades.c
@@ -0,0 +1,22 @@
+#include "funcionalidades.h"
+
+
+void juncaoLoopUnico(char *nomeArqVeic, char *nomeArqLinhas, char *campoVeiculo, char *campoLinha) {
+
+}
+
+void juncaoArquivoIndice(char *nomeArqVeic, char *nomeArqLinhas, char *campoVeiculo, char *campoLinha, char *nomeArqIndice) {
+
+}
+
+void ordenacaoVeiculos(char *nomeArqDesordenado, char *nomeArqOrdenado, char *campoOrdenacao) {
+
+}
+
+void ordenacaoLinhas(char *nomeArqDesordenado, char *nomeArqOrdenado, char *campoOrdenacao) {
+
+}
+
+void juncaoOrdenada(char *nomeArqVeic, char *nomeArqLinhas, char *campoVeiculo, char *campoLinha) {
+
+}
